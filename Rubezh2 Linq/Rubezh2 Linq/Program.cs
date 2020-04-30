92cc3bba4dff59cadca2ccd81f2b1670bccb5a97
alievkamil508 <alievkamil508@gmail.com>
2020-04-30 20:16:31 +0600

    ff
    ff

---
+++ Rubezh2 Linq/Rubezh2 Linq/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubezh2_Linq
+{
+    class School
+    {
+        public string Name { get; set; }
+       
+    }
+
+    class Class
+    {
+        public string Name { get; set; }
+        
+    }
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Class { get; set; }
+        public string School { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>
+            {
+                new Student{Name = "Ерланов Темирлан", Age = 7, Class = "1A", School = "Школа 1"},
+                new Student{Name = "Танашбаева Роза", Age = 8, Class = "1Б", School = "Школа 1" },
+                new Student{Name = "Серикова Алина", Age = 7, Class = "1Б", School = "Школа 1" },
+                new Student{Name = "Сабыржанов Даулет",Age = 7, Class = "1Б", School = "Школа 1" },
+                new Student{Name = "Каришев Самат", Age = 8, Class = "1Б", School = "Школа 2" },
+                new Student{Name = "Темников Александр", Age = 7, Class = "1A" , School = "Школа 1"},
+                new Student{Name = "Теззекбай Самат", Age = 8, Class = "1Б", School = "Школа 1" },
+                new Student{Name = "Седовласов Юри", Age = 7, Class = "1Б", School = "Школа 2" },
+                new Student{Name = "Джумабаев Алмабек", Age = 7, Class = "1A", School = "Школа 2" },
+                new Student{Name = "Жумабаева Инкар", Age = 8, Class = "1Б", School = "Школа 1" },
+                new Student{Name = "Казиханов Ерасыл", Age = 7, Class = "1A" , School = "Школа 1"}
+
+            };
+
+            List<School> school = new List<School>
+            {
+                new School{Name = "Школа 1"},
+                new School{Name = "Школа 1" },
+            };
+
+            List<Class> class1 = new List<Class>
+            {
+                new Class{Name = "1Б"},
+                new Class{Name = "1А" },
+            };
+
+
+            var result1 = students.Select(s => s.Name)
+                                  .OrderBy(n => n[1]);
+
+            foreach (var item in result1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------------------------");
+
+            var result2 = from student in students
+                         group student by student.School into g
+                         orderby g.Key
+                         select new { Name_of_School = g.Key, Count = g.Count() };
+
+            foreach (var item in result2)
+            {
+                Console.WriteLine("{0} - {1}", item.Name_of_School, item.Count);
+            }
+           
+        }
+    }
+}
---
+++ Rubezh2/Rubezh2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubezh2
+{
+    class EmployeePassport
+    {
+        public string Profession { get; set; }
+
+        public string Gender { get; set; }
+
+        public override int GetHashCode()
+        {
+
+            String str = Profession + "|" + Gender;
+            return str.GetHashCode();
+        }
+    }
+
+    class PassportFactory
+    {
+        private static PassportFactory _pfInstance;
+        private PassportFactory() { }
+
+        EmployeePassport bp = new EmployeePassport() { Profession = "", Gender = ""};
+
+        HashSet<EmployeePassport> _passport = new HashSet<EmployeePassport>();
+        public EmployeePassport GetPassport(string profession, string gender)
+        {
+
+            bp.Profession = profession;
+            bp.Gender = gender;
+
+            if (_passport.Contains(bp))
+            {
+                return _passport.First(p => p.GetHashCode() == bp.GetHashCode());
+            }
+            else
+            {
+                _passport.Add(bp);
+                return bp;
+            }
+        }
+
+        public static PassportFactory GetInstance()
+        {
+            if (_pfInstance == null)
+                _pfInstance = new PassportFactory();
+            return _pfInstance;
+        }
+    }
+
+
+    class Employee
+    {
+        EmployeePassport Passport;
+
+        public int Id { get; set; }
+
+        public void PrintPassport()
+        {
+            Console.Write(Id + " ");
+            Console.Write(Passport.Profession + " ");
+            Console.WriteLine(Passport.Gender);
+        }
+
+        public Employee(PassportFactory pf, int id, string profession, string gender)
+        {
+            Id = id;
+            Passport = pf.GetPassport(profession, gender);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PassportFactory pf = PassportFactory.GetInstance();
+            Random random = new Random();
+
+            for (int i = 0; i < 100000; i++)
+            {
+
+               
+                if(i%3 == 0)
+                {
+                    Employee b1 = new Employee(pf, i+1, "Бухгалтер", "муж");
+                    b1.PrintPassport();
+                }
+                else if(i% 3 == 1)
+                {
+                    Employee b1 = new Employee(pf, i+1, "Программист", "муж");
+                    b1.PrintPassport();
+                }
+                else if (i%3 == 2)
+                {
+                    Employee b1 = new Employee(pf, i+1, "Менеджер", "жен");
+                    b1.PrintPassport();
+                }
+                
+            }
+
+            
+            
+        }
+    }
+}
